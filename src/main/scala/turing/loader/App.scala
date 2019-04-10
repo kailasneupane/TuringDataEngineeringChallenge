61fdad86289a69757abebb50937a2dc2cdc891f3
kailasneupane <kailasneupane@gmail.com>
2019-04-10 23:58:48 +0545

    project ongoing

--- src/main/scala/turing/loader/App.scala
+++ src/main/scala/turing/loader/App.scala
@@ -1,16 +1,14 @@
 package turing.loader
 
-import java.io.File
-
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
-
-import sys.process._
 
 object App {
 
   val sparkConf = new SparkConf().setAppName("Practice").setMaster("local[*]")
   sparkConf.set("spark.hadoop.validateOutputSpecs", "false") // to override output
+  sparkConf.set("spark.hadoop.mapreduce.input.fileinputformat.input.dir.recursive", "true")
+
   val sparkContext = new SparkContext(sparkConf)
 
   def main(args: Array[String]): Unit = {
@@ -21,46 +19,44 @@ object App {
     /**
       * 1st. clone each repos
       */
-    var repoUrl = "https://github.com/kevinburke/hamms"
+    var repoUrl = "https://github.com/tensorflow/tensorflow"
     var repoName = repoUrl.substring(repoUrl.lastIndexOf("/"))
     var cloningPath: String = "output/repos"
     var pyProjectPath = cloningPath + repoName + "/*"
-    cloneRepo(repoUrl)
+    Utils.cloneRepoAndRetainPyFilesOnly(repoUrl)
 
 
     /**
       * 1. Number of lines of code [this excludes comments, whitespaces, blank lines].
+      *
       * 2. List of external libraries/packages used.
+      *
+      * 3. The Nesting factor for the repository: the Nesting factor is the average depth of a
+      * nested for loop throughout the code. (entire repository)
+      *
+      * 4. Code duplication: What percentage of the code is duplicated per file.
+      * If the same 4 consecutive lines of code (disregarding blank lines, comments,
+      * etc. other non code items) appear in multiple places in a file, all the occurrences
+      * except the first occurence are considered to be duplicates.
+      *
+      * 5. Average number of parameters per function definition in the repository.
+      *
+      * 6. Average Number of variables defined per line of code in the repository.
       */
-    var pyRdd: RDD[String] = sparkContext.textFile(pyProjectPath)
-    pyRdd.filter(x => !x.trim.startsWith("#") || !x.trim.isEmpty)
+    val pyRdd: RDD[String] = sparkContext.textFile(pyProjectPath).filter(x => !x.trim.startsWith("#") || !x.trim.isEmpty)
 
+
+    /**
+      * 1. Number of lines of code [this excludes comments, whitespaces, blank lines].
+      */
     println("No. of python lines in repo. = " + pyRdd.count())
 
     /**
-      * 1.
+      * 2. List of external libraries/packages used.
       */
 
 
     println("Process Completed!!!")
-  }
-
-  def cloneRepo(url: String, cloningPath: String = "output/repos/"): Unit = {
-
-    val directory = new File(cloningPath)
-    if (!directory.exists) {
-      directory.mkdir
-    }
-
-    val repoName = url.substring(url.lastIndexOf("/"))
-
-    println(repoName)
-
-    s"git clone $url $cloningPath$repoName --branch master" !
-
-    println("Repo \"" + repoName + "\" cloned.")
-
-    Utils.retainPyFilesOnly(cloningPath + repoName)
   }
 
 }
--- src/main/scala/turing/loader/Utils.scala
+++ src/main/scala/turing/loader/Utils.scala
@@ -1,17 +1,33 @@
 package turing.loader
 
-import scala.reflect.io.File
+
+import java.io.File
+
+import sys.process._
 
 /**
   * Created by kneupane on 4/10/19.
   */
 object Utils {
 
-  def retainPyFilesOnly(path: String): Unit = {
-    for {
-      files <- Option(new java.io.File(path).listFiles)
-      file <- files if !file.getName.endsWith(".py")
-    } file.delete()
+  private def retainPyFilesOnly(path: java.io.File): Unit = {
+    if (path.isDirectory)
+      path.listFiles.foreach(retainPyFilesOnly)
+    if (path.exists && !path.getName.endsWith(".py"))
+      path.delete()
+  }
+
+
+  def cloneRepoAndRetainPyFilesOnly(url: String, cloningPath: String = "output/repos/"): Unit = {
+    val directory = new java.io.File(cloningPath)
+    if (!directory.exists) {
+      directory.mkdir
+    }
+    val repoName = url.substring(url.lastIndexOf("/"))
+    s"git clone $url $cloningPath$repoName --branch master --single-branch" !
+
+    retainPyFilesOnly(new File(cloningPath + repoName))
+    println(s"git clone $repoName successful and only .py files retained.")
   }
 
 }
