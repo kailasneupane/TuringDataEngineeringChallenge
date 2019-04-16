b8e031b4fbdc8ae651f925f323a3083dbcc07530
kailasneupane <kailasneupane@gmail.com>
2019-04-16 23:44:32 +0545

    small change

--- src/main/scala/turing/lib/DuplicateFinder.scala
+++ src/main/scala/turing/lib/DuplicateFinder.scala
@@ -13,21 +13,21 @@ class DuplicateFinder(sparkContext: SparkContext) extends Serializable {
   def getConsecutive4LineDuplicateCount = duplicate4LinesAccumulator.value
 
   def duplicateAnalysisPerFile(fileStr: String): Unit = {
-    println("duplicate lines check ma chiryo")
+    //  println("duplicate lines check ma chiryo")
     val fileStrCRLF = fileStr.replaceAll("\r\n", "\n").replaceAll("\r", "\n")
     val lines = fileStrCRLF.split("\n").filter(x => !(x.trim.startsWith("#") || x.trim.isEmpty))
     var each4lines: ListBuffer[String] = ListBuffer()
 
     for (i <- 0 until (lines.length - 3)) {
-      each4lines += "{" + (lines(i) + "~" + lines(i + 1) + "~" + lines(i + 2) + "~" + lines(i + 3)) + "}"
+      each4lines += (lines(i) + lines(i + 1) + lines(i + 2) + lines(i + 3))
     }
 
-    each4lines.groupBy(fourLines => fourLines).mapValues(_.length).foreach(println)
+    // each4lines.groupBy(fourLines => fourLines).mapValues(_.length).foreach(println)
     var exclusiveDuplicateCount: Int = 0
     try {
       exclusiveDuplicateCount = each4lines.groupBy(fourLines => fourLines).mapValues(_.length).map(x => x._2).filter(x => x != 1).map(x => x - 1).reduce(_ + _)
     } catch {
-      case e1: UnsupportedOperationException => println("No duplicate here")
+      case e1: UnsupportedOperationException => ()
     }
     duplicate4LinesAccumulator.add(0l + exclusiveDuplicateCount)
   }
