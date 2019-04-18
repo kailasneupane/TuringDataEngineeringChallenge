c5f6f4e042df16b694a9202389c53b1f32c8c3c4
kailasneupane <kailasneupane@gmail.com>
2019-04-18 08:07:57 +0545

    project ongoing

--- src/main/scala/turing/test/TestPy.java
+++ src/main/scala/turing/test/TestPy.java
@@ -13,6 +13,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class TestPy {
 
@@ -21,7 +22,10 @@ public class TestPy {
         return new String(encoded, encoding);
     }
 
-    static String fileName = "/home/kneupane/work/projects/practice/turing_git_analysis/src/main/scala/turing/test/simple.py";
+    static String fileName = "/home/serrorist/IdeaProjects/turing_git_analysis/src/main/scala/turing/test/simple.py";
+
+
+    static String parens = "";
 
     public static void main(String[] args) throws IOException {
         //   System.out.println("ded de ded ed");
@@ -30,6 +34,8 @@ public class TestPy {
         Python3Lexer lexer = new Python3Lexer(CharStreams.fromString(source));
         Python3Parser parser = new Python3Parser(new CommonTokenStream(lexer));
 
+        Stack<String> forStack = new Stack<>();
+        ArrayList<String> list = new ArrayList<>();
 
         ParseTreeWalker.DEFAULT.walk(new Python3BaseListener() {
 
@@ -81,6 +87,7 @@ public class TestPy {
                 //   System.out.println("import from = "+ ctx.dotted_name().getText());
             }
 
+
             //for loop1
             @Override
             public void enterCompound_stmt(Python3Parser.Compound_stmtContext ctx) {
@@ -94,20 +101,83 @@ public class TestPy {
                     int startIndex = ctx.for_stmt().getStart().getCharPositionInLine();
                     int endLine = ctx.for_stmt().getStop().getLine();
 
-                    System.out.printf("(startLine, endLine, startIndex) = (%d, %d, %d)\n", startLine, endLine, startIndex);
+                    //System.out.printf("(startLine, endLine, startIndex) = (%d, %d, %d)\n", startLine, endLine, startIndex);
+                    System.out.println(/* ctx.getStart().getText() + */"(");
+                    System.out.println(endLine + ")");
+                    //System.out.println(ctx.getStop().getText());
                     //   System.out.printf("(endLine = %d)\n", endLine);
 
-                    System.out.println("****");
+                    list.add("(");
+                    list.add(endLine + ")");
                 } catch (Exception e) {
 
                 }
             }
 
-            @Override
-            public void enterAsync_funcdef(Python3Parser.Async_funcdefContext ctx) {
-                System.out.println();
-            }
         }, parser.file_input());
 
+        loopCounter(list);
+
+    }
+
+    private static void loopCounter(ArrayList<String> listArr) {
+        ArrayList<String> newList = listArr;
+
+        for (int i = 0; i < listArr.size() - 1; i++) {
+            for (int j = listArr.size() - 1; j >= 1; j--) {
+                if (listArr.get(i).equals("(")) {
+                    continue;
+                }
+                if (i!=j && listArr.get(i).equals(listArr.get(j))) {
+                    newList.set(j, listArr.get(j) + newList.get(i));
+                    newList.set(i, "");
+                    System.out.println("equal "+ listArr.get(i));
+                }
+            }
+        }
+
+        System.out.println("\n\n new list");
+        StringBuilder str = new StringBuilder("");
+        for (int i = 0; i < newList.size(); i++) {
+            System.out.println(newList.get(i));
+            str.append(newList.get(i));
+        }
+
+        System.out.println("***");
+        parens = str.toString().replaceAll("\n", "").replaceAll("[0-9]", "");
+        System.out.println(parens);
+
+        System.out.println(maxDepth(parens));
+    }
+
+    static int maxDepth(String S) {
+        int current_max = 0; // current count
+        int max = 0; // overall maximum count
+        int n = S.length();
+
+        // Traverse the input string
+        for (int i = 0; i < n; i++) {
+            if (S.charAt(i) == '(') {
+                current_max++;
+
+                // update max if required
+                if (current_max > max) {
+                    max = current_max;
+                }
+            } else if (S.charAt(i) == ')') {
+                if (current_max > 0) {
+                    current_max--;
+                } else {
+                    return -1;
+                }
+            }
+        }
+
+        // finally check for unbalanced string
+        if (current_max != 0) {
+            return -1;
+        }
+
+        return max;
     }
 }
--- src/main/scala/turing/test/simple.py
+++ src/main/scala/turing/test/simple.py
@@ -1,13 +1,12 @@
-def lol():
-
-    for i in [1,2,3]:
-        print("dd")
-        for z in [1,2]:
-            print(z)
-            print(z)
-            print(z)
+for i in [1,2,3]:
+    print("dd")
+    for z in [1,2]:
+        print(z)
+    for z in [1,2]:
+        print(z)
         for z in [1,2]:
             print(z)
 
-for z in [1,2]:
-    print(z)
+
+for x in [2,4]:
+    print(x)
